78327d145555034edac2fedd27c3b4f48984a5f0
WeiLiang <511684406@qq.com>
2018-01-29 13:13:50 +0900

    Added a Main and some functions

---
+++ src/Main.java
@@ -0,0 +1,11 @@
+public class Main {
+    public static void main(String[] args){
+        System.out.println("hello world!");
+    }
+
+    //public static void placeWalls(char[][] room, int numberOfWalls, Random randGen) {}
+    //public static void placeCheeses(int[][] cheesePositions, char[][] room, Random randGen) {}
+    //public static void printRoom(char[][] room, int[][] cheesePositions, int mouseX, int mouseY) {}
+    //public static int[] moveMouse(int mouseX, int mouseY, char[][] room, char move) {}
+    //public static boolean tryToEatCheese(int mouseX, int mouseY, int[][] cheesePositions) {}
+}
